af60124747bd872dfca033589439f2c7d2a07549
z888e <zoemhrt@gmail.com>
2022-09-26 09:48:22 +0200

    Initial Commit

---
+++ Mapper/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Mapper
+//
+//  Created by Zoé Hartman on 26/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mapper/MapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapperApp.swift
+//  Mapper
+//
+//  Created by Zoé Hartman on 26/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct MapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
